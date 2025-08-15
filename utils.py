d3a10271155e29804fadf1b92a4e16bcee6a14fc
Dhia Naouali <dhia12naouali@gmail.com>
2025-08-16 00:21:33 +0100

    loading train & test sets seperatly

--- utils.py
+++ utils.py
@@ -1,10 +1,25 @@
 import os
 import numpy as np
 
-def load_data():
+def _load_data():
     features_path = os.path.join("dummy_data", "features.npy")
     targets_path = os.path.join("dummy_data", "targets.npy")
     
     x = np.load(features_path)
     y = np.load(targets_path)
     return x, y
+
+def split(*args, test_size=.2):
+    splitted_args = []
+    for array in args:
+        n = len(array)
+        n_test = int(test_size * n)
+        splitted_args.extend([
+            array[n_test:],
+            array[:n_test]
+        ])
+        
+    return splitted_args
+
+def load_data():
+    return split(*_load_data())
